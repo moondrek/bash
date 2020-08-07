8f7a7452ae1b5b1098000ebd73aef02227d8b091
David Moon <moon.kyoungje@gmail.com>
2020-08-07 15:59:27 -0400

    adds headjs file

--- bash.js
+++ bash.js
@@ -19,6 +19,10 @@ process.stdin.on("data", (data) => {
     cat(done, cmd.split(" ")[1]);
   } else if (cmd.split(" ")[0] === "curl") {
     curl(done, cmd.split(" ")[1]);
+  } else if (cmd === "date") {
+    done(Date());
+  } else if (cmd.split(" ")[0] === "echo") {
+    done(cmd.split(" ").slice(1).join(" "));
   } else {
     process.stdout.write("You typed : " + cmd + "\n");
   }
