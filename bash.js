53584d1fb6c76bc3c0636d9a9df5ff718c46b07f
Jeff_Lee_0228 <suam3282@gmail.com>
2020-08-07 11:11:17 -0500

    working on modules

---
+++ bash.js
@@ -0,0 +1,9 @@
+process.stdout.write("prompt > ");
+process.stdin.on("data", (data) => {
+  const cmd = data.toString().trim();
+  // process.stdout.write("You typed : " + cmd);
+  if (cmd === "pwd") {
+    process.stdout.write(process.cwd());
+  }
+  process.stdout.write("\nprompt > ");
+});
