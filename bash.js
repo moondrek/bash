bc72831fe1905dd12fd5b7ed3210a6d9989c7e5f
David Moon <moon.kyoungje@gmail.com>
2020-08-07 14:54:38 -0400

    implements curl

--- bash.js
+++ bash.js
@@ -1,6 +1,7 @@
 const pwd = require("./pwd");
 const ls = require("./ls");
 const cat = require("./cat");
+const curl = require("./curl");
 
 process.stdout.write("prompt > ");
 process.stdin.on("data", (data) => {
@@ -12,6 +13,8 @@ process.stdin.on("data", (data) => {
     ls();
   } else if (cmd.split(" ")[0] === "cat") {
     cat(cmd.split(" ")[1]);
+  } else if (cmd.split(" ")[0] === "curl") {
+    curl(cmd.split(" ")[1]);
   } else {
     process.stdout.write("You typed : " + cmd + "\n");
   }
--- curl.js
+++ curl.js
@@ -1,1 +1,12 @@
 const request = require("request");
+
+function curl(url) {
+  request(url, function (error, response, body) {
+    console.error("error:", error); // Print the error if one occurred
+    console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
+    console.log("body:", body); // Print the HTML for the Google homepage.
+    process.stdout.write("\nprompt > ");
+  });
+}
+
+module.exports = curl;
